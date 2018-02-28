abfb011b23ad4e88697881dc87e316b6cf62c156
chandrugouda <cushychandru@gmail.com>
2018-02-28 16:07:04 +0530

    adding examples

---
+++ Report Example/Report/data/datafile.js
@@ -0,0 +1,33 @@
+var data = [
+		{
+			"_count" : "1",
+			"_class" : "TestReport.Test1",
+			"_mothod" : "testCase1",
+			"_time" : "20",
+			"_status" : "SUCCESS",
+			"_exception" : "--"
+		},
+		{
+			"_count" : "2",
+			"_class" : "TestReport.Test2",
+			"_mothod" : "testCase2",
+			"_time" : "30",
+			"_status" : "FAILED",
+			"_exception" : "Error Explanation : / by zero\u003cbr\u003e Class Name :  TestReport.TestngAnnotation\u003cbr\u003e File Name :  TestngAnnotation.java\u003cbr\u003e Error Line Number :  31"
+		}, {
+			"_count" : "3",
+			"_class" : "TestReport.Test3",
+			"_mothod" : "testCase3",
+			"_time" : "25",
+			"_status" : "SKIPED",
+			"_exception" : "Skipping this exception"
+		},
+		{
+			"_count" : "4",
+			"_class" : "TestReport.Test4",
+			"_mothod" : "testCase4",
+			"_time" : "35",
+			"_status" : "SUCCESS",
+			"_exception" : "--"
+		}
+		];
